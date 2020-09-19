09843de6902d376d6213f0a268c6c6653eaeb693
wakitomo <58131438+wakitomo@users.noreply.github.com>
2020-09-19 12:23:15 +0900

    Add files via upload

---
+++ web-site 製作物/js/main.js
@@ -0,0 +1,24 @@
+$(document).ready(function(){
+    $nav = $('.nav');
+    $toggleCollapse = $('.toggle-collapse'); 
+
+    $toggleCollapse.click(function(){
+        $nav.toggleClass('collapse');
+    });
+
+    $(function(){
+        $(window).scroll(function (){
+            $('.fadein').each(function(){
+                var targetElement = $(this).offset().top;
+                var scroll = $(window).scrollTop();
+                var windowHeight = $(window).height();
+                if (scroll > targetElement - windowHeight + 200){
+                    $(this).css('opacity','1');
+                    $(this).css('transform','translateY(0)');
+                }
+            });
+        });
+    });
+    
+});
+
